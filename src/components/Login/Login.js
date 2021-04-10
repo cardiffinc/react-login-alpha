006799578aed986fd2c746d56283567cffbd1f96
sjcardiff <61339666+cardiffinc@users.noreply.github.com>
2021-04-10 17:27:52 +0530

    Login Component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import artwork from '../../staticAssets/login-artwork.png'
-import { Avatar, Button, Checkbox, FormControlLabel, TextField } from '@material-ui/core';
+import facebook from '../../staticAssets/facebook.png'
+import instagram from '../../staticAssets/instagram.png'
+import linkedin from '../../staticAssets/linkedin.png'
+import twitter from '../../staticAssets/twitter.png'
+import { Avatar, Button, Checkbox, Divider, FormControlLabel, TextField } from '@material-ui/core';
+import { useHistory } from 'react-router';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -33,9 +39,10 @@ const useStyles = makeStyles((theme) => ({
         marginRight: '5em'
     },
     authInput: {
-        marginTop: 15
+        marginTop: 15,
     },
     checkFormControlLabel: {
+        marginTop: 25,
         margin: '0 5em',
         width: 'fit-content',
         display: 'block'
@@ -47,12 +54,37 @@ const useStyles = makeStyles((theme) => ({
         marginTop: 25,
         marginLeft: '5em',
         marginRight: '5em',
-        width: '-webkit-fill-available'
+        width: '-webkit-fill-available',
+        backgroundColor: '#ff4747',
+        textTransform: 'none'
+    },
+    divider: {
+        marginTop: 25,
+        marginBottom: 25,
     }
   }));
 
 function Login() {
-    const classes = useStyles()
+    const classes = useStyles();
+    const history = useHistory();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [disableButton, setDisableButton] = useState(true);
+    const [error, setError] = useState(null);
+
+    const handleLogin = event => {
+        event.preventDefault();
+        if(email === 'mail@mail.com' && password === 'password@2021'){
+            history.push('/')
+        } else setError('Email or Password is incorrect')
+    }
+
+    useEffect(() => {
+        setError(null);
+        if(email !== '' && password !== ''){
+            setDisableButton(false)
+        } else setDisableButton(true)
+    }, [email, password])
 
     return (
         <div className={classes.root}>
@@ -63,39 +95,63 @@ function Login() {
                             Welcome Back!
                         </h1>
                         <span>Sign in using your socials</span>
-                        <Grid container justify="center" className={classes.socialGrid} spacing='2'>
+                        <Grid container justify="center" className={classes.socialGrid} spacing={2}>
                             <Grid item xs={1}>
-                                <Avatar alt="Remy Sharp" src="../../logo.svg" />
+                                <Avatar alt="Facebook" src={facebook} />
                             </Grid>
                             <Grid item xs={1}>
-                                <Avatar alt="Remy Sharp" src="../../logo.svg" />
+                                <Avatar alt="Instagram" src={instagram} />
                             </Grid>
                             <Grid item xs={1}>
-                                <Avatar alt="Remy Sharp" src="../../logo.svg" />
+                                <Avatar alt="LinkedIn" src={linkedin} />
                             </Grid>
                             <Grid item xs={1}>
-                                <Avatar alt="Remy Sharp" src="../../logo.svg" />
+                                <Avatar alt="Twitter" src={twitter} />
                             </Grid>
                         </Grid>
                         <span>or use your email and password</span>
-                        <div className={classes.inputWrapper}>
-                            <TextField id="outlined-basic" label="Email" variant="outlined" fullWidth className={classes.authInput}/>
-                            <TextField id="outlined-basic" label="Password" variant="outlined" fullWidth className={classes.authInput}/>
+                        
+                        <form onSubmit={handleLogin}>
+                            <div className={classes.inputWrapper}>
+                                <TextField 
+                                    id="email-input" 
+                                    label="Email" 
+                                    variant="outlined" 
+                                    fullWidth 
+                                    color='secondary'
+                                    className={classes.authInput} 
+                                    onChange={event=>setEmail(event.target.value)}/>
+                                <TextField 
+                                    id="password-input" 
+                                    label="Password" 
+                                    variant="outlined" 
+                                    fullWidth
+                                    color='secondary'
+                                    className={classes.authInput} 
+                                    onChange={event=>setPassword(event.target.value)}/>
+                            </div>
+                            <FormControlLabel
+                                    control={
+                                    <Checkbox
+                                        name="checkedB"
+                                        color="secondary"
+                                        className={classes.checkbox}
+                                    />
+                                    }
+                                    label="Keep me logged in"
+                                    className={classes.checkFormControlLabel}
+                            />
+                            <Button id="signin-button" variant="contained" size="large" color="primary" className={classes.authButton} type="submit" disabled={disableButton}>
+                                Sign In
+                            </Button>
+                            {error && 
+                            <p>{error}
+                            </p>}
+                        </form>
+                        <Divider className={classes.divider} />
+                        <div>
+                            Don't have an account? <Link to='/signup' style={{color: "#ff4747"}}>Sign Up!</Link>
                         </div>
-                        <FormControlLabel
-                                control={
-                                <Checkbox
-                                    name="checkedB"
-                                    color="primary"
-                                    className={classes.checkbox}
-                                />
-                                }
-                                label="Keep me logged in"
-                                className={classes.checkFormControlLabel}
-                        />
-                        <Button variant="contained" size="large" color="primary" className={classes.authButton}>
-                            Signin
-                        </Button>
                     </div>
                 </Grid>
                 <Grid item xs={4}>
